485770422fe78d7759601e974c15e3aaaae2c55c
Gabriel Lamoglia <43991948+lam0glia@users.noreply.github.com>
2019-09-21 19:52:35 -0300

    Registrar usuario
    Passando pela classe de validacao

--- Finder/Views/Register.xaml.cs
+++ Finder/Views/Register.xaml.cs
@@ -1,60 +1,30 @@
-﻿using Finder.DAO;
-using Finder.Models;
+using Finder.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Finder.Views
 {
     /// <summary>
-    /// Interaction logic for Register.xaml
+    /// Lógica interna para Register.xaml
     /// </summary>
     public partial class Register : Window
     {
-        private User user = new User();
-
         public Register()
         {
             InitializeComponent();
         }
 
-        private void TxtNome_TextChanged(object sender, TextChangedEventArgs e)
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
         {
-            user.Name = txtNome.Text;
-        }
-
-        private void TxtEmail_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            user.Email = txtEmail.Text;
-        }
-
-        private void TxtSenha_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            user.Password = txtSenha.Text;
-        }
-
-        private void BtnCadastrar_Click(object sender, RoutedEventArgs e)
-        {
-            
-            if(user != null)
+            try
             {
-                UserDAO.Save(user);
-                MessageBox.Show("Cadastrado com sucesso!", "Sucesso", MessageBoxButton.OK);
+                UserService.Register(txtName.Text, txtEmail.Text, txtPassword.Password, txtPass.Password);
             }
-            else
+            catch (Exception exception)
             {
-                MessageBox.Show("Nenhum campo deve estar vazio!", "Falha", MessageBoxButton.OK);
+                MessageBox.Show(exception.Message, "Falha ao entrar", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
+
     }
 }
